847a7c87cf2689e4cf51791353e11ea3f2a90caa
Erik Opinaldo <erikopinaldo@gmail.com>
2022-06-12 12:44:15 -0400

    save queries to local storage

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,22 @@ if (!localStorage.getItem('parcelHistoryArr')) {
 	localStorage.setItem('parcelHistoryArr', JSON.stringify([]))
   }
 
+// Grab current parcelHistoryArr so that new queries can be pushed to this array
+let parcelHistoryArr = JSON.parse(localStorage.getItem('parcelHistoryArr'))
+console.log(parcelHistoryArr)
+
+// Click triggers the query to be saved to local storage
+document.querySelector('button').addEventListener('click', addTrackingToLocalStorage)
+
+function addTrackingToLocalStorage() {
+	const trackingNumber = document.querySelector('input').value
+	console.log(trackingNumber)
+	
+	console.log('click!!')
+	parcelHistoryArr.push(trackingNumber)
+	localStorage.setItem('parcelHistoryArr', JSON.stringify(parcelHistoryArr))
+}
+
 (function($) {
 
 	var	$window = $(window),
